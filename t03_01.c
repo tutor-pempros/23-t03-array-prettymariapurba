8c31f789db05ef8f0e23777e06ef21dce46eb4a4
prettymariapurba <prettymaria110406@gmail.com>
2024-02-06 21:38:59 +0700

    12s23048

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,27 @@
-// NIM - NAMA
+// 12S23042 - PRETTY MARIA 
 
 #include <stdio.h>
 
-int main(int _argc, char **_argv)
-{
-
-  // your code here
-
-  return 0;
+int main(int _argc, char **_argv) 
+  {
+    int n;
+    scanf("%d", &n);
+    int i, numbers[n], min, max;
+    for(i = 0; i < n; i++) {
+    scanf("%d", &numbers[i]);
+    }
+    for(i = 0; i < n; i++) {
+      if(numbers[i] < min) {
+      min = numbers[i];
+      }
+      else if(numbers[i] > max) {
+      max = numbers[i];
+        }
+    }
+    // Output
+    printf("%d\n", min);
+    printf("%d\n", max);
+    return 0;
 }
+
+
